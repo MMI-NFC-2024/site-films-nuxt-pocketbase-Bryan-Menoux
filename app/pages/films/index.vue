d4f90ac9e349eb79d93fca0b0c9963826898261a
bryan-menoux <bryan.menoux@edu.univ-fcomte.fr>
2026-03-02 15:20:42 +0100

    modification de l'affichage de la page des films

--- app/pages/films/index.vue
+++ app/pages/films/index.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const nuxtApp = useNuxtApp()
+const films = await nuxtApp.$pb.collection("films").getFullList()
+const filmsWithUrls = films.map((film) => ({
+    ...film,
+    afficheUrl: nuxtApp.$pb.files.getURL(film, film.affiche)
+}))
+</script>
 <template>
-    <h1 class="font-bold text-4xl"  >films</h1>
+    <h1 class="font-bold text-4xl">films</h1>
+    <ul>
+        <li v-for="film in filmsWithUrls" class="flex max-w-full">
+            <div>
+                <h2 class="font-bold text-2xl">{{ film.titre }}</h2>
+                <img :src="film.afficheUrl" :alt="`Affiche de ${film.titre}`" class="w-64 h-auto">
+            </div>
+        </li>
+    </ul>
 </template>
